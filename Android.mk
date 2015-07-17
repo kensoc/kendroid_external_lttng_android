3323e334a9896d593f2b265a618d44950ebf6506
Hardik Patel <hardik.patel@live.com>
2015-07-17 11:41:15 -0700

    Added empty android mk file

---
+++ Android.mk
@@ -0,0 +1,11 @@
+#
+#==============================================================================
+#	File:		Android.mk
+#	Author:		Hardik Patel
+#	Date:		07/17/2015
+#	Version:	0.1
+#	Usage:		This make file builds all lttng and its modules for 
+#			android build system.
+#	copyright:
+#===============================================================================
+#
